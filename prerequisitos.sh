f5a0129270cbdaa08c3a2dbd30ac9eed8b64184d
Alejandro Vargas <61161340+malevarro@users.noreply.github.com>
2024-02-03 11:33:56 -0500

    Update prerequisitos.sh

--- prerequisitos.sh
+++ prerequisitos.sh
@@ -1,5 +1,5 @@
 sudo apt-get -y install     apt-transport-https     ca-certificates     curl     software-properties-common
-
+#hola
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
